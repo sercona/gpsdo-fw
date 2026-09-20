ab50e1f71a2f57ca12f19ce9bca84b09916bec52
linux-works <sercona@users.noreply.github.com>
2026-09-20 16:06:59 -0700

    removed some unneeded screen draws

--- src/menu.c
+++ src/menu.c
@@ -1710,15 +1710,15 @@ void menu_run()
             }
             if(did_pps && did_pwm)
             {
-                LCD_Clear();
-                LCD_Puts(0, 0, "PPS&PWM ");
-                LCD_Puts(0, 1, " DONE!  ");
+	      //LCD_Clear();
+                //LCD_Puts(0, 0, "PPS&PWM ");
+                //LCD_Puts(0, 1, " DONE!  ");
             }
             else if(did_pps)
             {
-                LCD_Clear();
-                LCD_Puts(0, 0, "  PPS   ");
-                LCD_Puts(0, 1, "SYNCED! ");
+	      //LCD_Clear();
+                //LCD_Puts(0, 0, "  PPS   ");
+                //LCD_Puts(0, 1, "SYNCED! ");
             }
             else if(did_pwm)
             {
@@ -1727,7 +1727,7 @@ void menu_run()
                 LCD_Puts(0, 1, EEPROM_AUTO_SAVE ? " SAVED! " : "  SET!  ");
             }
             //HAL_Delay(1500);   // getting in the way, not needed
-            LCD_Clear();
+            //LCD_Clear();
             menu_force_redraw();
         }
         bool new_ppb_lock_status = frequency_is_stable(ppb_lock_threshold);
